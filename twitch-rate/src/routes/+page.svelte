e4697e515b76c65dbf039ecfab7112a27d73f3ff
Toluwase Afolayan <tolu.afolayan27@gmail.com>
2023-07-01 16:12:15 -0400

    removed skeleton things, and added a modal that we can continue to expand on

--- twitch-rate/src/routes/+page.svelte
+++ twitch-rate/src/routes/+page.svelte
@@ -6,8 +6,8 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>Streamer Ranker</h1>
+<section class="flex flex-col">
+		<h1>Streamer Ranker</h1>
 </section>
 
 <style>
