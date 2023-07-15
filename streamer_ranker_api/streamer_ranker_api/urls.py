620bc1496f18ec7a4ab2f9a9be7e060993d942a5
Toluwase Afolayan <tolu.afolayan27@gmail.com>
2023-07-14 20:04:45 -0400

    add api/v1 to api path

--- streamer_ranker_api/streamer_ranker_api/urls.py
+++ streamer_ranker_api/streamer_ranker_api/urls.py
@@ -34,6 +34,6 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/v1/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
