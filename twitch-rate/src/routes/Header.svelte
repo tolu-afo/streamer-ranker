6b4b0da0de3997ad6a27c9780568e642fd648428
Toluwase Afolayan <tolu.afolayan27@gmail.com>
2023-07-21 21:06:42 -0400

    added three panels that describie functionality of website

--- twitch-rate/src/routes/Header.svelte
+++ twitch-rate/src/routes/Header.svelte
@@ -1,8 +1,6 @@
 <script>
 	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
-	import { Button, Modal, Label, Input, Checkbox } from 'flowbite-svelte';
+	import {Button, Modal, Label, Input, Checkbox } from 'flowbite-svelte';
 
 	let homeIconPath = 'images/monke.png';
 
@@ -12,7 +10,7 @@
 
 <header class="flex justify-between px-[3.125rem] p-2 text-white bg-[#5C17C5]">
 	<div class="w-8 rounded-full">
-		<a href="https://kit.svelte.dev">
+		<a href="/">
 			<img src={homeIconPath} alt="toluafMONKE" />
 		</a>
 	</div>
@@ -34,17 +32,13 @@
 		</ul>
 	</nav>
 	<div class="flex p-2">
-		<!-- TODO: have this trigger a login modal -->
 		<button on:click={() => (loginModal = true)} class="flex gap-2">
 			<svg fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg" height="1.5rem" viewBox="0 0 512 512"
 				><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
 					d="M406.5 399.6C387.4 352.9 341.5 320 288 320H224c-53.5 0-99.4 32.9-118.5 79.6C69.9 362.2 48 311.7 48 256C48 141.1 141.1 48 256 48s208 93.1 208 208c0 55.7-21.9 106.2-57.5 143.6zm-40.1 32.7C334.4 452.4 296.6 464 256 464s-78.4-11.6-110.5-31.7c7.3-36.7 39.7-64.3 78.5-64.3h64c38.8 0 71.2 27.6 78.5 64.3zM256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-272a40 40 0 1 1 0-80 40 40 0 1 1 0 80zm-88-40a88 88 0 1 0 176 0 88 88 0 1 0 -176 0z"
-				/></svg
-			>
+				/></svg>
 			<span>Sign In</span>
 		</button>
-
-		<!-- <Modal title="Sign In or Create an Account" bind:open={defaultModal} autoclose outsideclose /> -->
 	</div>
 
 	<!-- Main modal -->
@@ -70,7 +64,7 @@
 			<div class="text-sm font-medium text-gray-500 dark:text-gray-300 text-center">
 				Not registered? <a
 					href="/"
-					class="text-purple-800 hover:underline dark:text-primary-500"
+					class="text-purple-800 hover:underline dark:text-purple-500"
 					on:click={() => {
 						signInModal = true;
 						loginModal = false;
