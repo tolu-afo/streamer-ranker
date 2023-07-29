0e4c20400d7f9b5bf31464c8b28e7e883bef9489
afolayan-t <54455919+afolayan-t@users.noreply.github.com>
2023-07-29 17:27:36 -0400

    [11] added email input field, sitemap, and modified footer. (#17)

--- twitch-rate/src/routes/+layout.svelte
+++ twitch-rate/src/routes/+layout.svelte
@@ -2,6 +2,12 @@
 	import Header from './Header.svelte';
 	import './styles.css';
 	import '../app.css';
+	let homeIconPath = 'images/monke.png';
+
+	// placeholder for sitemap
+	let items = ['item 1', 'item 2', 'item 3', 'item 4', 'item 5']
+	let lists = ['List 1', 'List 2', 'List 3', 'List 4']
+
 </script>
 
 <div class="app">
@@ -11,8 +17,32 @@
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://www.twitch.tv/toluafo">ToluAfo</a> to learn about being a human.</p>
+	<footer class="flex flex-col">
+		<div id="sitemap" class="mx-auto bg-[#5C17C5] w-full mt-[-1rem] py-4 flex justify-evenly">
+			<div id="logo">
+				<h1 class="text-xl text-white font-bold">Streamer Ranker</h1>
+				<div class="w-24 mx-auto rounded-full">
+					<a href="/">
+						<img src={homeIconPath} alt="toluafMONKE" />
+					</a>
+				</div>
+			</div>
+			<div id="links" class="flex justify-between text-white font-bold text-lg">
+				{#each lists as list}
+					<div id={list} class="">
+						<h1>{list}</h1>
+						<ul class="mx-8">
+							{#each items as item}
+								<li><a href="/">{item}</a></li>
+							{/each}
+						</ul>
+					</div>
+				{/each}
+			</div>
+		</div>
+		<div class="bg-black text-white font-bold w-full text-center py-2">
+			<p>StreamerRanker All Rights Reserved &copy;</p>
+		</div>
 	</footer>
 </div>
 
